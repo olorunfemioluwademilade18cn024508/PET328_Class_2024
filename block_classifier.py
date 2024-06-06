12bcd63d478b3994c824fb368790a75ca215ed39
olorunfemioluwademilade18cn024508 <105019424+olorunfemioluwademilade18cn024508@users.noreply.github.com>
2024-06-06 22:31:33 +0100

    Update block_classifier.py

--- block_classifier.py
+++ block_classifier.py
@@ -1,21 +1,27 @@
 #TTOWG
 
-# A script to classify gridblocks in a discretized
-# oil reservoir; based on the position of the gridblock
-# relative to the reservoir boundaries.
+# Define the function classify_block
+def classify_block(i, j, nx, ny):
+    """
+    Classify gridblocks in a discretized oil reservoir based on the position
+    of the gridblock relative to the reservoir boundaries.
 
-# Receive inputs from user:
-i = int(input('What is the column index of the gridblock?'))
-j = int(input('What is the row index of the gridblock?'))
-nx = int(input('How may columns are there in the discerized reservoir?'))
-ny = int(input('How may rows are there in the discerized reservoir?'))
+    Parameters:
+    i (int): Column index of the gridblock
+    j (int): Row index of the gridblock
+    nx (int): Total number of columns in the discretized reservoir
+    ny (int): Total number of rows in the discretized reservoir
 
-# Classify gridblock
-if (i == 1 and j == 1) or (i == 1 and j == ny) or (i == nx and j == 1) or (i == nx and j == ny):
-    block_cat = 'IV'
-elif j==1 or j==ny:
-    block_cat = 'II'
-elif i==1 or i==nx:
-    block_cat = 'III'
-else:
-    block_cat = 'I'
+    Returns:
+    str: Block category
+    """
+    if (i == 1 and j == 1) or (i == 1 and j == ny) or (i == nx and j == 1) or (i == nx and j == ny):
+        block_cat = 'IV'
+    elif j == 1 or j == ny:
+        block_cat = 'II'
+    elif i == 1 or i == nx:
+        block_cat = 'III'
+    else:
+        block_cat = 'I'
+    
+    return block_cat
